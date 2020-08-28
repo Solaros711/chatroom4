6958d5894a82536446466af541443de0e089734b
Solaros711 <spurdy885@gmail.com>
2020-08-27 17:24:22 -0700

    Added update

--- static/components.js
+++ static/components.js
@@ -1,25 +1,38 @@
-const {createUsernameHandler} = require('./handlers')
-
 const yo = require('yo-yo')
+const moment = require('moment')
+const { createUsernameHandler } = require('./handlers')
+const styleMessage = require('./style-message')
 
-function Message (message) {
-    return yo`<div id="message"><li class="msg"><span style="color: maroon"><b><i>${message.nick}:</i></b></span>  ${message.text}</li></div>`
+function Message (message, state) {
+  const styleObj = styleMessage(message.text)
+  console.log(message.date)
+  const momentDate = moment(message.date).format('h:mm:ss a')
+  return yo`<div class="message${message.username === state.name ? ' end' : ' start'}" >
+    <li class="msg" style="align: right">
+      <span style="${message.username === state.name ? 'color: #ba6c48' : 'color: #8f2543'}"><b><i>${message.username}:</i></b></span>
+      <span style="${styleObj.outStyle}">${styleObj.outText}</span>
+      <span> -- ${momentDate}</span>
+    </li>
+  </div>`
 }
 
-function Messages (messages) {
-    return yo`<ul id="messages" style="list-style-type: none">${messages.map(message => Message(message))}</ul>`
+function Messages (messages, state) {
+  return yo`<ul id="messages" style="list-style-type: none">
+    ${messages.map(message => Message(message, state))}
+    <div></div>
+  </ul>`
 }
 
-function Username (body, username) {
-    const username_el = yo`<div id="username">
+function Username (body, state) {
+  const usernameEl = yo`<div id="username">
         <input id="username_input" placeholder="Enter username..." type="text">
         </div>`
-        username_el.appendChild(yo`<button onclick=${createUsernameHandler(username_el, body, username)} id="username_btn">Submit</button>`)
-    return username_el
+  usernameEl.appendChild(yo`<button onclick=${createUsernameHandler(usernameEl, body, state)} id="username_btn">Submit</button>`)
+  return usernameEl
 }
 
 function selectRoom (rooms) {
-    return yo`<select id="room">
+  return yo`<select id="room">
     <optgroup label="Pick a room..." >
     <option value="" id="drop-1" disabled="disabled" selected="selected" style="color: grey" hidden>Rooms...</option>
     ${rooms.map(room => {
@@ -30,8 +43,8 @@ function selectRoom (rooms) {
 }
 
 module.exports = {
-    Message,
-    Messages,
-    Username,
-    selectRoom
+  Message,
+  Messages,
+  Username,
+  selectRoom
 }
--- static/handlers.js
+++ static/handlers.js
@@ -1,13 +1,11 @@
-function createUsernameHandler (el, body, username) {
-    return function (){
-        console.log(username)
-        username.name = document.querySelector('#username_input').value
-        console.log(username)
-        body.removeChild(el)
-        username.display_.style.display = 'block'
-    }
+function createUsernameHandler (el, body, state) {
+  return function () {
+    state.name = document.querySelector('#username_input').value
+    body.removeChild(el)
+    state.display_.style.display = 'block'
+  }
 }
 
 module.exports = {
-    createUsernameHandler
+  createUsernameHandler
 }
